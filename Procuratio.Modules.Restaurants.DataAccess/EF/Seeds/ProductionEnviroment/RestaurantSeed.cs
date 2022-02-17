7304315f1b0e7cf101c4152bab104e7c1f864ffd
Tomas Gavagnin <tomasgavagnin@gmail.com>
2022-02-17 18:40:32 -0300

    Finished

--- Procuratio.Modules.Restaurants.DataAccess/EF/Seeds/ProductionEnviroment/RestaurantSeed.cs
+++ Procuratio.Modules.Restaurants.DataAccess/EF/Seeds/ProductionEnviroment/RestaurantSeed.cs
@@ -11,7 +11,7 @@ namespace Procuratio.Modules.Restaurants.DataAccess.EF.Seeds.ProductionEnviromen
         {
             if (restaurantDbSet.Any()) return;
 
-            restaurantDbSet.Add(new Domain.Entities.Restaurant()
+            restaurantDbSet.AddRange(new Domain.Entities.Restaurant()
             {
                 Id = 1,
                 Name = "El DOMO",
@@ -23,6 +23,101 @@ namespace Procuratio.Modules.Restaurants.DataAccess.EF.Seeds.ProductionEnviromen
                     {
                         Id = 1,
                         RestaurantId = 1,
+                        Address = "Direccion falsa 5600",
+                        Phone = "03034274171",
+                        Instagram = "https://www.instagram.com/procuratiosoftware/",
+                        Location = string.Empty,
+                        DateWithdrawn = null,
+                        BranchSettings = new List<BranchSetting>()
+                        {
+                            new BranchSetting()
+                            {
+                                BranchId = 1,
+                                SettingId = 1,
+                                UnconstrainedValue = "true"
+                            },
+                            new BranchSetting()
+                            {
+                                BranchId = 1,
+                                SettingId = 2,
+                                UnconstrainedValue = "true"
+                            }
+                        }
+                    },
+                    new Domain.Entities.Branch()
+                    {
+                        Id = 2,
+                        RestaurantId = 1,
+                        Address = "Direccion falsa 5600",
+                        Phone = "03034274171",
+                        Instagram = "https://www.instagram.com/procuratiosoftware/",
+                        Location = string.Empty,
+                        DateWithdrawn = null,
+                        BranchSettings = new List<BranchSetting>()
+                        {
+                            new BranchSetting()
+                            {
+                                BranchId = 1,
+                                SettingId = 1,
+                                UnconstrainedValue = "true"
+                            },
+                            new BranchSetting()
+                            {
+                                BranchId = 1,
+                                SettingId = 2,
+                                UnconstrainedValue = "true"
+                            }
+                        }
+                    }
+                }
+            }, 
+            new Domain.Entities.Restaurant()
+            {
+                Id = 2,
+                Name = "La vieja esquina",
+                Slogan = "Los mejores asados",
+                Image = string.Empty,
+                Branches = new List<Domain.Entities.Branch>()
+                {
+                    new Domain.Entities.Branch()
+                    {
+                        Id = 3,
+                        RestaurantId = 2,
+                        Address = "Marcelo T. de Alvear 1715",
+                        Phone = "03034274171",
+                        Instagram = "https://www.instagram.com/procuratiosoftware/",
+                        Location = string.Empty,
+                        DateWithdrawn = null,
+                        BranchSettings = new List<BranchSetting>()
+                        {
+                            new BranchSetting()
+                            {
+                                BranchId = 1,
+                                SettingId = 1,
+                                UnconstrainedValue = "true"
+                            },
+                            new BranchSetting()
+                            {
+                                BranchId = 1,
+                                SettingId = 2,
+                                UnconstrainedValue = "true"
+                            }
+                        }
+                    }
+                }
+            },
+            new Domain.Entities.Restaurant()
+            {
+                Id = 3,
+                Name = "Silveria",
+                Slogan = "Los mejores lomitos",
+                Image = string.Empty,
+                Branches = new List<Domain.Entities.Branch>()
+                {
+                    new Domain.Entities.Branch()
+                    {
+                        Id = 4,
+                        RestaurantId = 2,
                         Address = "Direccion falsa 5600",
                         Phone = "03034274171",
                         Instagram = "https://www.instagram.com/procuratiosoftware/",
--- Procuratio.Modules.Securities.DataAccess/EF/Seeds/MicrosoftIdentity/MicrosoftIdentitySeeder.cs
+++ Procuratio.Modules.Securities.DataAccess/EF/Seeds/MicrosoftIdentity/MicrosoftIdentitySeeder.cs
@@ -13,25 +13,35 @@ namespace Procuratio.Modules.Security.DataAccess.EF.Seeds.MicrosoftIdentity
 {
     internal class MicrosoftIdentitySeeder
     {
+        private static readonly string[] roles = new string[] { "Administrador", "Chef", "Mozo", "Gerente" };
+
         internal static async Task EnsureSeedDataAsync(IServiceScope scope)
         {
             RoleManager<Role> roleManager = scope.ServiceProvider.GetService<RoleManager<Role>>();
             CustomUserManager customUserManager = scope.ServiceProvider.GetService<CustomUserManager>();
 
-            string[] roles = new string[] { "Administrador", "Chef", "Mozo", "Gerente" };
+            await CreateRoles(roleManager);
+            await CreateAdminUser(customUserManager);
+            await CreateDefaultUsers(customUserManager);
+        }
 
+        private static async Task CreateRoles(RoleManager<Role> roleManager)
+        {
             foreach (string role in roles)
             {
                 if (!await roleManager.RoleExistsAsync(role))
                 {
-                    await roleManager.CreateAsync(new Role 
-                    { 
+                    await roleManager.CreateAsync(new Role
+                    {
                         Name = role,
                         NormalizedName = role.ToUpper()
                     });
                 }
             }
+        }
 
+        private static async Task CreateAdminUser(CustomUserManager customUserManager)
+        {
             string defaultUserName = "orion";
 
             if (await customUserManager.FindByNameIgnoringQueryFiltersAsync(defaultUserName) is null)
@@ -52,6 +62,123 @@ namespace Procuratio.Modules.Security.DataAccess.EF.Seeds.MicrosoftIdentity
                 if (checkUser.Succeeded)
                 {
                     await customUserManager.AddToRoleAsync(user, roles[0]);
+                }
+            }
+        }
+
+        private static async Task CreateDefaultUsers(CustomUserManager customUserManager)
+        {
+            string[] userNames = new string[] 
+            {
+                "chef1",
+                "mozo1",
+                "gerente1",
+                "chef2",
+                "mozo2",
+                "gerente2",
+                "chef3",
+                "mozo3",
+                "gerente3",
+                "chef4",
+                "mozo4",
+                "gerente4",
+            };
+
+            string[] names = new string[]
+            {
+                "Benjamin",
+                "Bruno",
+                "Diego",
+                "Federico",
+                "Joaquin",
+                "Leonardo",
+                "Luciano",
+                "Martin",
+                "Matias",
+                "Valentino",
+                "Agustina",
+                "Agustina"
+            };
+
+            string[] surnames = new string[]
+            {
+                "Gonzalez",
+                "Rodriguez",
+                "Gomez",
+                "Fernandez",
+                "Lopez",
+                "Martinez",
+                "Diaz",
+                "Perez",
+                "Sanchez",
+                "Romero",
+                "Garcia",
+                "Sosa"
+            };
+
+            string[] passwords = new string[]
+            {
+                "chef1",
+                "mozo1",
+                "gerente1",
+                "chef2",
+                "mozo2",
+                "gerente2",
+                "chef3",
+                "mozo3",
+                "gerente3",
+                "chef4",
+                "mozo4",
+                "gerente4",
+            };
+
+            int[] branchIds = new int[]
+            {
+                1,
+                1,
+                1,
+                2,
+                2,
+                2,
+                3,
+                3,
+                3,
+                4,
+                4,
+                4
+            };
+
+            int auxRoleCount = 1;
+
+            for (int i = 0; i < 12; i++)
+            {
+                if (await customUserManager.FindByNameIgnoringQueryFiltersAsync(userNames[i]) is null)
+                {
+                    User user = new();
+                    user.UserName = userNames[i];
+                    user.Name = names[i];
+                    user.Surname = surnames[i];
+                    user.UserStateId = (short)UserState.State.Active;
+                    user.Email = string.Empty;
+                    user.NormalizedEmail = string.Empty;
+                    user.PhoneNumber = "3534274171";
+                    user.ProfilePicture = string.Empty;
+                    user.BranchId = branchIds[i];
+
+                    IdentityResult checkUser = await customUserManager.CreateAsync(user, passwords[i]);
+
+                    if (checkUser.Succeeded)
+                    {
+                        await customUserManager.AddToRoleAsync(user, roles[auxRoleCount]);
+                    }
+                    else
+                    {
+                        throw new Exception(checkUser.Errors.ToString());
+                    }
+
+                    auxRoleCount += 1;
+
+                    if (auxRoleCount == 4) { auxRoleCount = 1; }
                 }
             }
         }
