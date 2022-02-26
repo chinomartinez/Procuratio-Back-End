0e2c715708a7acf2a6b1c630470f62aeff3997bc
Tomas Gavagnin <tomasgavagnin@gmail.com>
2022-02-26 15:29:12 -0300

    MinorChangesOnHub

--- Procuratio.API.Modules.Notification.API/Services/CustomerMenuSender.cs
+++ Procuratio.API.Modules.Notification.API/Services/CustomerMenuSender.cs
@@ -69,9 +69,7 @@ namespace Procuratio.API.Modules.Notification.API.Services
 
         public async Task SendNotificationFromCustomerToWaiter(string fromUser, string orderKey)
         {
-            List<string> tables = await _orderModuleAPI.GetTablesForWaiterNotification(orderKey);
-
-            if (tables is null || tables.Count == 0) { throw new TablesNotFoundException(); }
+            List<string> tables = await GetTablesOfTheOrder(orderKey);
 
             CustomerNotificationDTO customerNotificationDTO = new();
 
@@ -85,27 +83,12 @@ namespace Procuratio.API.Modules.Notification.API.Services
                 customerNotificationDTO.Message = $"El comensal de la mesa { tables.First() } te esta llamando.";
             }
 
-            int? waiterIdOfTheOrder = await _orderModuleAPI.GetWaiterIdOfTheOrder(orderKey);
-
-            if (waiterIdOfTheOrder is null || waiterIdOfTheOrder <= 0) { throw new WaiterOfflineException(); }
-
-            foreach (ConnectedUsers conectedUser in _connectedUsers)
-            {
-                if (conectedUser.WaiterId == waiterIdOfTheOrder)
-                {
-                    await Clients.Client(conectedUser.ConectionId).SendAsync("waiterResponse", customerNotificationDTO, fromUser);
-                    return;
-                }
-            }
-
-            throw new WaiterOfflineException();
+            SendUserToUserMessage("waiterResponse", customerNotificationDTO, orderKey, fromUser);
         }
 
         public async Task SendBillNotificationFromCustomerToWaiter(string fromUser, string orderKey)
         {
-            List<string> tables = await _orderModuleAPI.GetTablesForWaiterNotification(orderKey);
-
-            if (tables is null || tables.Count == 0) { throw new TablesNotFoundException(); }
+            List<string> tables = await GetTablesOfTheOrder(orderKey);
 
             CustomerNotificationDTO customerNotificationDTO = new();
 
@@ -119,27 +102,12 @@ namespace Procuratio.API.Modules.Notification.API.Services
                 customerNotificationDTO.Message = $"El comensal de la mesa { tables.First() } quiere la cuenta.";
             }
 
-            int? waiterIdOfTheOrder = await _orderModuleAPI.GetWaiterIdOfTheOrder(orderKey);
-
-            if (waiterIdOfTheOrder is null || waiterIdOfTheOrder <= 0) { throw new WaiterOfflineException(); }
-
-            foreach (ConnectedUsers conectedUser in _connectedUsers)
-            {
-                if (conectedUser.WaiterId == waiterIdOfTheOrder)
-                {
-                    await Clients.Client(conectedUser.ConectionId).SendAsync("waiterBillResponse", customerNotificationDTO, fromUser);
-                    return;
-                }
-            }
-
-            throw new WaiterOfflineException();
+            SendUserToUserMessage("waiterBillResponse", customerNotificationDTO, orderKey, fromUser);
         }
 
         public async Task SendNewOrderNotificationFromCustomerToWaiter(string fromUser, string orderKey)
         {
-            List<string> tables = await _orderModuleAPI.GetTablesForWaiterNotification(orderKey);
-
-            if (tables is null || tables.Count == 0) { throw new TablesNotFoundException(); }
+            List<string> tables = await GetTablesOfTheOrder(orderKey);
 
             CustomerNotificationDTO customerNotificationDTO = new();
 
@@ -153,6 +121,13 @@ namespace Procuratio.API.Modules.Notification.API.Services
                 customerNotificationDTO.Message = $"El comensal de la mesa { tables.First() } agrego artículos al pedido.";
             }
 
+            SendUserToUserMessage("waiterNewOrderResponse", customerNotificationDTO, orderKey, fromUser);
+        }
+
+        public string GetConnectionId() => Context.ConnectionId;
+
+        private async void SendUserToUserMessage(string methodName, CustomerNotificationDTO customerNotificationDTO, string orderKey, string fromUser)
+        {
             int? waiterIdOfTheOrder = await _orderModuleAPI.GetWaiterIdOfTheOrder(orderKey);
 
             if (waiterIdOfTheOrder is null || waiterIdOfTheOrder <= 0) { throw new WaiterOfflineException(); }
@@ -161,7 +136,7 @@ namespace Procuratio.API.Modules.Notification.API.Services
             {
                 if (conectedUser.WaiterId == waiterIdOfTheOrder)
                 {
-                    await Clients.Client(conectedUser.ConectionId).SendAsync("waiterNewOrderResponse", customerNotificationDTO, fromUser);
+                    await Clients.Client(conectedUser.ConectionId).SendAsync(methodName, customerNotificationDTO, fromUser);
                     return;
                 }
             }
@@ -169,6 +144,13 @@ namespace Procuratio.API.Modules.Notification.API.Services
             throw new WaiterOfflineException();
         }
 
-        public string GetConnectionId() => Context.ConnectionId;
+        private async Task<List<string>> GetTablesOfTheOrder(string orderKey)
+        {
+            List<string> tables = await _orderModuleAPI.GetTablesForWaiterNotification(orderKey);
+
+            if (tables is null || tables.Count == 0) { throw new TablesNotFoundException(); }
+
+            return tables;
+        }
     }
 }
